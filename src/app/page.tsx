57aff4680818e2abd3964d735bdb0f7a2bff74b3
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-01-01 09:48:28 +0700

    refactor: editing phone number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function HomePage() {
         <div className="flex items-center justify-center h-screen px-4">
             <Card className="p-5">
                 <CardTitle className="text-center">
-                    Maaf, Jokilagila masih dalam tahap pengembangan yaa 
+                    Maaf, Jokilagila masih dalam tahap pengembangan yaa
                 </CardTitle>
                 <CardContent>
                     <p className="text-center mt-5">
                         Jika ada pertanyaan, silahkan hubungi kami di
                     </p>
                     <div className="flex items-center justify-center mt-5">
-                        <Link href="https://wa.me/6281217952403" target="blank">
+                        <Link href="https://wa.me/6289697083392" target="blank">
                             <Button>
                                 <PhoneIcon className="mr-2" size={16} /> Nomor Telepon Kami
                             </Button>
